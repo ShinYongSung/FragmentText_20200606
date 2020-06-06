11c212a232e6f30d4be08a532ddcf1fb61a0a65c
ShinYongSUng <newxm@naver.com>
2020-06-06 15:08:14 +0900

    학생 어댑터 뼈대 작업 진행

---
+++ app/src/main/java/my/shin/fragmenttext_20200606/adapters/StudentAdapter.kt
@@ -0,0 +1,32 @@
+package my.shin.fragmenttext_20200606.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import my.shin.fragmenttext_20200606.R
+import my.shin.fragmenttext_20200606.datas.Student
+
+class StudentAdapter(context: Context, resId : Int, list: List<Student>) : ArrayAdapter<Student>(context, resId, list){
+
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        tempRow?.let {
+            // null이 아닌경우
+
+        }.let {
+            // null인 경우
+            tempRow = inf.inflate(R.layout.student_list_item,null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
+
+
+}
