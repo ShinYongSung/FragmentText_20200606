a1c2238d6d9a677ca0d32d4781ade51c629fa1f5
ShinYongSUng <newxm@naver.com>
2020-06-06 16:03:19 +0900

    페이지별 제목 추가

--- app/src/main/java/my/shin/fragmenttext_20200606/adapters/MyViewPagerAdapter.kt
+++ app/src/main/java/my/shin/fragmenttext_20200606/adapters/MyViewPagerAdapter.kt
@@ -7,6 +7,18 @@ import my.shin.fragmenttext_20200606.fragments.StudentListFragment
 import my.shin.fragmenttext_20200606.fragments.myProfileFragment
 
 class MyViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        if (position == 0) {
+            return "내 프로필"
+        }
+
+        else {
+            return "수강생 목록"
+        }
+
+    }
+
     override fun getItem(position: Int): Fragment {
         if (position ==0) {
             return myProfileFragment()
