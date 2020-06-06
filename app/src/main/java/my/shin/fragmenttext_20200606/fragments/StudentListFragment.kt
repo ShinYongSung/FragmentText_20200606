e22edd92b9367b55ab82b9a4375c7791c76993f8
ShinYongSUng <newxm@naver.com>
2020-06-06 15:15:51 +0900

    어댑터 객체 등 다양한 활동

--- app/src/main/java/my/shin/fragmenttext_20200606/fragments/StudentListFragment.kt
+++ app/src/main/java/my/shin/fragmenttext_20200606/fragments/StudentListFragment.kt
@@ -1,13 +1,21 @@
 package my.shin.fragmenttext_20200606.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_student_list.*
 import my.shin.fragmenttext_20200606.R
+import my.shin.fragmenttext_20200606.adapters.StudentAdapter
+import my.shin.fragmenttext_20200606.datas.Student
 
 class StudentListFragment : Fragment() {
+
+    val studentlist = ArrayList<Student>()
+
+    lateinit var mStudentAdapter: StudentAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +28,17 @@ class StudentListFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        studentlist.add(Student("조경진", 1988, true))
+        studentlist.add(Student("김광철", 1966, true))
+        studentlist.add(Student("김재환", 1993, true))
+        studentlist.add(Student("박수정", 1994, false))
+        studentlist.add(Student("신용성", 1988, true))
+        studentlist.add(Student("엄곤지", 1984, false))
+        studentlist.add(Student("이승원", 1978, true))
+        studentlist.add(Student("이현호", 1981, true))
+
+        mStudentAdapter = StudentAdapter(activity as Context, R.layout.student_list_item, studentlist)
+        studentListView.adapter = mStudentAdapter
     }
 }
